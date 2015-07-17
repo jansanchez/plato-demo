d7d763d598c426f6d480726f30ea00d140ffedea
Jan Sanchez <joejansanchez@gmail.com>
2015-07-17 00:27:07 -0500

    feat(demo): agrego los archivos a pasarle pruebas de complejidad

---
+++ dist/index.js
@@ -0,0 +1,49 @@
+/*
+*
+* Project     : Plato - Demo
+* Author      : @jansanchez
+* Description : Project for learn to use the npm package called plato
+* 
+*/
+
+StringTwo = function(originalText, caseType){
+	this.text = originalText;
+	this.output = '';
+	this.to(caseType);
+	return this.getText();
+}
+
+StringTwo.prototype.to = function(caseType) {
+	switch(caseType){
+		case 'lower':
+			this.toLower();
+		break;
+		case 'upper':
+			this.toUpper();
+		break;
+		default:
+			this.toLower();
+		break;
+	};
+};
+
+StringTwo.prototype.toLower = function() {
+	this.text = this.text.toString().toLowerCase();
+	return this.text;
+};
+
+StringTwo.prototype.toUpper = function() {
+	this.text = this.text.toString().toUpperCase();
+	return this.text;
+};
+
+StringTwo.prototype.getText = function() {
+	this.output = this.text;
+	return this.output;
+};
+
+
+var newText = new StringTwo('mInUsCulA', 'lower');
+
+console.log(newText.output);
+
